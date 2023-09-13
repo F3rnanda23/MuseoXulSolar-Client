95e02f156749d6bbf4684fffa0371c9ec1aa1ffb
RSLastra <raul.slastra@gmail.com>
2023-09-12 23:26:21 -0600

    login avance

--- front-end/src/views/Login/Login.jsx
+++ front-end/src/views/Login/Login.jsx
@@ -2,8 +2,10 @@ import { LoginForm } from "../../components/forms/loginForm/loginForm"
 
 export function Login() {
     return(
-        <div>
-            <LoginForm/>
+        <div className="h-screen bg-orange-300">
+            <div className="container mx-auto pt-20">
+                <LoginForm/>
+            </div>
         </div>
     )
 
