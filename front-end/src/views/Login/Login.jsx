8dbff4001546c609e60b47b52b91ee789e8351c4
RSLastra <raul.slastra@gmail.com>
2023-09-13 23:37:26 -0600

    login estilo

--- front-end/src/views/Login/Login.jsx
+++ front-end/src/views/Login/Login.jsx
@@ -3,11 +3,11 @@ import { LoginForm } from "../../components/forms/loginForm/loginForm"
 export function Login() {
     return(
         <div className="flex w-full h-screen ">
-            <div className="w-full flex align-center justify-center md:w-1/2">
+            <div className="w-full bg-gradient-to-tr from-orange-500 to-yellow-200 flex align-center justify-center md:w-1/2 py-12">
                 <LoginForm/>
             </div>
-            <div className="hidden md:flex h-full w-1/2 items-center justify-center bg-gray-300">
-                <p className="text-6xl font-semibold text-red-500  animate-bounce">Diego se la come</p>
+            <div className="hidden md:flex h-full w-1/2 items-center justify-center bg-gray-600">
+                <div className="w-60 h-60 bg-gradient-to-tr from-orange-500 to-yellow-200 rounded-full animate-bounce"></div>
             </div>
         </div>
     )
