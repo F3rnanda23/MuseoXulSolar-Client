825fdc8b6674daa3695ffd5494faa0b46e18922f
RSLastra <raul.slastra@gmail.com>
2023-09-18 01:49:54 -0600

    estilo validacion

--- front-end/src/views/Login/Login.jsx
+++ front-end/src/views/Login/Login.jsx
@@ -3,11 +3,11 @@ import { LoginForm } from "../../components/forms/loginForm/loginForm"
 export function Login() {
     return(
         <div className="flex w-full h-screen ">
-            <div className="w-full bg-gradient-to-tr from-orange-500 to-yellow-200 flex align-center justify-center md:w-1/2 py-12">
+            <div className="w-full flex align-center justify-center md:w-1/2 py-12">
                 <LoginForm/>
             </div>
             <div className="hidden md:flex h-full w-1/2 items-center justify-center bg-gray-600">
-                <div className="w-60 h-60 bg-gradient-to-tr from-orange-500 to-yellow-200 rounded-full animate-bounce"></div>
+                <div className=""></div>
             </div>
         </div>
     )
