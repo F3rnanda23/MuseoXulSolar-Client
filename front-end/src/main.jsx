194ec41cbbe2fb30ff837452fa91f5b03b3a8bba
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-09-20 17:10:43 -0300

    redux persist

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,16 +1,20 @@
 
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store/store';
+import { store, persistor } from './redux/store/store';
 import App from './App.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}> {/* Agrega PersistGate */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- front-end/src/redux/store/store.js
+++ front-end/src/redux/store/store.js
@@ -1,12 +1,23 @@
 import { createStore, applyMiddleware,compose } from 'redux';
+import {persistStore, persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import reducer from '../reducer/reducer';
 import thunkMiddleware from 'redux-thunk';
 
 const composeEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: "root",  // Clave para el almacenamiento
+  storage,  // El almacenamiento que deseas utilizar (por ejemplo, localStorage)
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = createStore(
-  reducer,
+  persistedReducer,
   composeEnhancer(applyMiddleware(thunkMiddleware))   
 );
 
-export default store;
+const persistor = persistStore(store)
+
+export {store, persistor};
