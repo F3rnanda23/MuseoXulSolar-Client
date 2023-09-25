b114cf4336fc2d863579bde29da049ce27f82f4f
diegoalparra <diegoalparra08@gmail.com>
2023-09-25 08:15:53 -0500

    ruta de pago donaciones

--- front-end/src/views/Donations/Donations.jsx
+++ front-end/src/views/Donations/Donations.jsx
@@ -18,7 +18,7 @@ const Donations = () => {
         const price = donationAmount; 
 
         try {
-            const response = await axios.post('https://localhost:3001/pagar', {
+            const response = await axios.post('https://client-xul-solar.vercel.app/pagar', {
                 description: `Donativos - $${donationAmount}`,
                 price: price,
                 quantity: 1,
