a0242ce91e4890801c078fb38625ff5d8b47090f
diegoalparra <diegoalparra08@gmail.com>
2023-09-25 08:14:23 -0500

    comments

--- front-end/src/views/Donations/Donations.jsx
+++ front-end/src/views/Donations/Donations.jsx
@@ -18,7 +18,7 @@ const Donations = () => {
         const price = donationAmount; 
 
         try {
-            const response = await axios.post('https://server-xul-solar.vercel.app/pagar', {
+            const response = await axios.post('https://localhost:3001/pagar', {
                 description: `Donativos - $${donationAmount}`,
                 price: price,
                 quantity: 1,
