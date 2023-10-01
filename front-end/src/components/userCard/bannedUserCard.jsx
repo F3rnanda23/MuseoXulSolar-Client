bda93d2a1e5937d3be1eb212c909df4621b8c170
diegoalparra <diegoalparra08@gmail.com>
2023-09-30 22:51:46 -0500

    borrado logico ok

--- front-end/src/components/userCard/bannedUserCard.jsx
+++ front-end/src/components/userCard/bannedUserCard.jsx
@@ -10,18 +10,23 @@ const BannedUserCard = ({ banned }) => {
     const dispatch = useDispatch();
 
     const { id, email } = banned;
-    console.log(id);
-    
+   
     const handleRestoreUser = (id) => {
         dispatch(restoreUser(id))
     };
 
     return ( 
         <div>
-            <Card title="Banned User">
+            <Card title="Banned User" className="max-w-lg max-h-30 p-1 mt-10 mx-auto mb-5 font-bold">
+               <div className="flex flex-row">
+                <div>
                 <Text>{id}</Text><br/>
                 <Metric>{email}</Metric>
+                </div>
+               <div className="flex flex-col ml-auto mr-2 justify-center">
                 <Button onClick={() => handleRestoreUser(id)} className="max-h-10 mt-auto my-1 hover:bg-gray-600">Desbloquear</Button>
+               </div>
+               </div>
             </Card>
         </div>
      );
