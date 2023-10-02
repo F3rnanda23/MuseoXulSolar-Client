88ed9e0da2e8d0d1b24c5bfa104db1ad32da301c
RSLastra <raul.slastra@gmail.com>
2023-10-02 11:02:19 -0600

    boton admin

--- front-end/src/views/AdminDash/AdminRoute.jsx
+++ front-end/src/views/AdminDash/AdminRoute.jsx
@@ -2,7 +2,7 @@ import Cookies from "universal-cookie"
 import { Navigate } from "react-router-dom"
 
 
-export const AdminRoute = () => {
+export const AdminRoute = ({element}) => {
     const cookie = new Cookies()
 
     const admin = cookie.get('admin')
