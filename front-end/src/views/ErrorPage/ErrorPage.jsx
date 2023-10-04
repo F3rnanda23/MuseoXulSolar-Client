392090b7b2ac33a5806ebb67888b8cd5a3bb660c
diegoalparra <diegoalparra08@gmail.com>
2023-10-04 08:54:03 -0500

    rutas destacados

--- front-end/src/views/ErrorPage/ErrorPage.jsx
+++ front-end/src/views/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,21 @@
+import img from '../../imagenes/actividades/error.png'
+import backgroundImage from '../../imagenes/background/bg1.png'
+
 const ErrorPage = () => {
+
+    const backgroundStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      
+      };
+
+      
+
     return ( 
-        <div>
-            Error
+        <div className="flex flex-col items-center justify-center h-screen" style={backgroundStyle}>
+           <h3 className='font-semibold text-xl mb-5'>Esta pagina no existe, por favor vuelve a la página de inicio</h3>
+           <img src={img} alt="Error" />
         </div>
      );
 }
