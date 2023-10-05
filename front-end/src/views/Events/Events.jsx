30a57090cd54e42f32a55ddcad9188f09c01348b
RSLastra <raul.slastra@gmail.com>
2023-10-04 20:42:32 -0600

    carrusel eventos, imagen membresia

--- front-end/src/views/Events/Events.jsx
+++ front-end/src/views/Events/Events.jsx
@@ -4,13 +4,42 @@ import phoneIcon from "../../imagenes/footer/llamada-telefonica.png";
 import twitterLogo from "../../imagenes/footer/twitter.png";
 import envelopeIcon from "../../imagenes/footer/sobre.png";
 import imagenbg from "../../imagenes/background/bg1.png";
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { FormattedMessage } from 'react-intl';
+import { useState } from 'react';
+import imagenes from "./imageness";
+
+
 const Events = () => {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  function slideLeft() {
+    const slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 500;
+  }
+
+  function slideRight() {
+    const slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 500;
+  }
+
+
+  function handleImageClick(image) {
+    setSelectedImage(image);
+  }
+
+  function handleCloseClick() {
+    setSelectedImage(null);
+  }
+
   const backgroundStyle = {
     backgroundImage: `url(${imagenbg})`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
   };
+
+
   return (
     <div className="bg-gray-100 ">
       <main
@@ -58,6 +87,59 @@ const Events = () => {
             </div>
           </p>
         </div>
+      
+
+      <div className="my-8">
+        <div className="relative flex flex-col">
+
+          <div className="relative flex items-center">
+            <MdChevronLeft
+              className="opacity-50 cursor-pointer hover:opacity-100"
+              size={40}
+              onClick={slideLeft}
+            />
+
+            <div
+              id="slider"
+              className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+            >
+              {imagenes.map((image, index) => (
+                <img
+                  className="w-auto sm:w-[220px] max-h-72 mt-6 inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300"
+                  key={index}
+                  src={image.img}
+                  alt={image.name}
+                  onClick={() => handleImageClick(image)}
+                />
+              ))}
+            </div>
+
+            <MdChevronRight
+              className="opacity-50 cursor-pointer hover:opacity-100"
+              size={40}
+              onClick={slideRight}
+            />
+          </div>
+          {selectedImage && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+              <div className="max-w-3xl p-4 bg-gray-500 rounded-lg shadow-lg">
+                <img
+                  src={selectedImage.img}
+                  alt={selectedImage.name}
+                  className="w-full  max-h-[400px]"
+
+                />
+                <button
+                  className="mt-4 p-2 bg-orange-200 text-gray-100 rounded hover:bg-gray-100 hover:text-orange-200"
+                  onClick={handleCloseClick}
+                >
+                  Cerrar
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
       </main>
     </div>
   );
---
+++ front-end/src/views/Events/imageness.js
@@ -0,0 +1,47 @@
+
+const imagenes = [
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470535/Eventos/IMG_3732_rczgci.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470534/Eventos/Museo_Xul_Solar_2022-Foto_Carlos_FurmanDSC_5296_ojxe0g.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470534/Eventos/Museo_Xul_Solar-22-9-22-Foto_Carlos_Furman-DSC_5507_lmbxir.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470534/Eventos/Museo_Xul_Solar_2022-Foto_Carlos_FurmanDSC_5278_nczjry.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470533/Eventos/IMG_2096_wwc6so.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470533/Eventos/IMG_0705_daviqt.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470533/Eventos/E9DBA024-84E0-40BB-98D6-F1AF9A08588F_ab0jua.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470533/Eventos/3797A646-5E7E-43DA-BD37-0A47345F2BB9_rjhp7p.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470532/Eventos/B04AAEDE-C6A3-41FF-8154-32CB90A3F9DD_lypoct.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470531/Eventos/27109BBD-F463-4B82-9638-3A9FC85B8AB3_2_p0vjk6.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470529/Eventos/cef9fc3d-e2e9-4d61-b4a0-1dbba2884a4d_ha9kdc.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470529/Eventos/90e0c37b-ab23-47a6-b79b-21267b5b0a80_ilhf5d.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470529/Eventos/cb9d9c42-c66e-4b84-bb79-c754fd6fda95_ta1qso.jpg'
+    },
+    {
+        img:'https://res.cloudinary.com/dtsmy1ksn/image/upload/v1696470529/Eventos/5e28672a-040f-40ea-8ab2-1d6c98678970_zrfgpp.jpg'
+    },
+]
+
+export default imagenes;
